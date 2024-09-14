390d8b6aadd345f777a86f5203cc12d8bea79822
nghiakma <lam47788@gmail.com>
2024-09-14 19:44:14 +0700

    coursemodel

--- models/user.model.ts
+++ models/user.model.ts
@@ -2,107 +2,107 @@ require("dotenv").config();
 import mongoose, { Document, Model, Schema } from "mongoose";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import { courseSchema, ICourse } from "./course.model";
 
 const emailRegexPattern: RegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
+courseSchema
 export interface IUser extends Document {
-    name: string;
-    email: string;
-    password: string;
-    avatar: {
-      public_id: string;
-      url: string;
-    };
-    role: string;
-    isVerified: boolean;
-    courses: Array<{ courseId: string }>;
-    cart: Array<{ courseId: string }>;
-    comparePassword: (password: string) => Promise<boolean>;
-    SignAccessToken: () => string;
-    SignRefreshToken: () => string;
-  }
-  
-  
-  const userSchema: Schema<IUser> = new mongoose.Schema(
-    {
-      name: {
-        type: String,
-        required: [true, "Vui lòng nhập tên của bạn"],
-      },
-      email: {
-        type: String,
-        required: [true, "Vui lòng nhập email của bạn"],
-        validate: {
-          validator: function (value: string) {
-            return emailRegexPattern.test(value);
-          },
-          message: "Vui lòng nhập email hợp lệ",
-        },
-        unique: true,
-      },
-      password: {
-        type: String,
-        minlength: [6, "Mật khẩu phải chứa ít nhất 6 kí tự"],
-        select: false,
-      },
-      avatar: {
-        public_id: String,
-        url: String,
-      },
-      role: {
-        type: String,
-        default: "user",
-      },
-      isVerified: {
-        type: Boolean,
-        default: false,
-      },
-      courses: [
-        {
-          courseId: String,
-        },
-      ],
-      cart: [
-        {
-          courseId: String
-        }
-      ]
+  name: string;
+  email: string;
+  password: string;
+  avatar: {
+    public_id: string;
+    url: string;
+  };
+  role: string;
+  isVerified: boolean;
+  courses: Array<{ courseId: string }>;
+  cart: Array<{ courseId: string }>;
+  comparePassword: (password: string) => Promise<boolean>;
+  SignAccessToken: () => string;
+  SignRefreshToken: () => string;
+}
+
+
+const userSchema: Schema<IUser> = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Vui lòng nhập tên của bạn"],
     },
-    { timestamps: true }
-  );
-  
-  // Mã hóa mật khẩu trước khi lưu
-  //bảo mật mật khẩu bằng cách mã hóa trước khi lưu vào database
-  userSchema.pre<IUser>("save", async function (next) {
-    if (!this.isModified("password")) {
-      next();
-    }
-    this.password = await bcrypt.hash(this.password, 10);
+    email: {
+      type: String,
+      required: [true, "Vui lòng nhập email của bạn"],
+      validate: {
+        validator: function (value: string) {
+          return emailRegexPattern.test(value);
+        },
+        message: "Vui lòng nhập email hợp lệ",
+      },
+      unique: true,
+    },
+    password: {
+      type: String,
+      minlength: [6, "Mật khẩu phải chứa ít nhất 6 kí tự"],
+      select: false,
+    },
+    avatar: {
+      public_id: String,
+      url: String,
+    },
+    role: {
+      type: String,
+      default: "user",
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    courses: [
+      {
+        courseId: String,
+      },
+    ],
+    cart: [
+      {
+        courseId: String
+      }
+    ]
+  },
+  { timestamps: true }
+);
+
+// Mã hóa mật khẩu trước khi lưu
+//bảo mật mật khẩu bằng cách mã hóa trước khi lưu vào database
+userSchema.pre<IUser>("save", async function (next) {
+  if (!this.isModified("password")) {
     next();
+  }
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+// Tạo access token
+userSchema.methods.SignAccessToken = function () {
+  return jwt.sign({ id: this._id }, process.env.ACCESS_TOKEN || "", {
+    expiresIn: "5m",
   });
-  
-  // Tạo access token
-  userSchema.methods.SignAccessToken = function () {
-    return jwt.sign({ id: this._id }, process.env.ACCESS_TOKEN || "", {
-      expiresIn: "5m",
-    });
-  };
-  
-  // Tạo refresh token
-  userSchema.methods.SignRefreshToken = function () {
-    return jwt.sign({ id: this._id }, process.env.REFRESH_TOKEN || "", {
-      expiresIn: "3d",
-    });
-  };
-  
-  // compare password
-  userSchema.methods.comparePassword = async function (
-    enteredPassword: string
-  ): Promise<boolean> {
-    return await bcrypt.compare(enteredPassword, this.password);
-  };
-  
-  const userModel: Model<IUser> = mongoose.model("User", userSchema);
-  
-  export default userModel;
-  
+};
+
+// Tạo refresh token
+userSchema.methods.SignRefreshToken = function () {
+  return jwt.sign({ id: this._id }, process.env.REFRESH_TOKEN || "", {
+    expiresIn: "3d",
+  });
+};
+
+// compare password
+userSchema.methods.comparePassword = async function (
+  enteredPassword: string
+): Promise<boolean> {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const userModel: Model<IUser> = mongoose.model("User", userSchema);
+
+export default userModel;
